58421ab0fab5a55befcaca94d13b3686a44cf038
EvgeniyBokovFIT <75373950+EvgeniyBokovFIT@users.noreply.github.com>
2021-09-16 02:18:24 +0700

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -8,14 +8,14 @@ static const int BUF_SIZE = 256;
 
 void *print(void *arg) {
     for(int i = 0; i < 10; i++)
-    printf("%s %d\n", (char*)arg, i+1);
+    printf("%s %d\n", (char*)arg, i + 1);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t threadID;
     int errnum;
-    errnum = pthread_create(&threadID, NULL, print, "CHILDREN");
+    errnum = pthread_create(&threadID, NULL, print, (void*)"CHILDREN");
     if (errnum != THREAD_CREATED_SUCCESSFULLY) {
         char str[BUF_SIZE];
         strerror_r(errnum, str, BUF_SIZE);
