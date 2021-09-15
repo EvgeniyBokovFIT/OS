5c4fafaec17bf257b87cc39b37ce700db5a34a6c
EvgeniyBokovFIT <75373950+EvgeniyBokovFIT@users.noreply.github.com>
2021-09-16 02:15:09 +0700

    Create lab1.c

---
+++ lab1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+static const int THREAD_CREATED_SUCCESSFULLY = 0;
+static const int BUF_SIZE = 256;
+
+void *print(void *arg) {
+    for(int i = 0; i < 10; i++)
+    printf("%s %d\n", (char*)arg, i+1);
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t threadID;
+    int errnum;
+    errnum = pthread_create(&threadID, NULL, print, "CHILDREN");
+    if (errnum != THREAD_CREATED_SUCCESSFULLY) {
+        char str[BUF_SIZE];
+        strerror_r(errnum, str, BUF_SIZE);
+        fprintf(stderr, "%s", str);
+        exit(EXIT_FAILURE);
+    }
+
+    print("PARENT");
+    pthread_exit(NULL);
+    return (EXIT_SUCCESS);
+}
